9521771a2b8561803621e7b446835c0a2e138c3d
Adrian Etenyi <etadriano2@gmail.com>
2020-02-23 19:33:51 +0300

    update script modifications

--- js/scripts.js
+++ js/scripts.js
@@ -6,24 +6,24 @@ $(function(){
 
 $(document).ready(function () {
   // this is a custom pre-loading screen which allows for images and other assets to load as well as provide a nice visual effect
-  $("#Loader").fadeOut(5000);
-  $("#head").addClass("onLoad").fadeIn(1000);
+  $("#Loader").fadeOut(3000);
+  $("#head").addClass("onLoad").fadeIn(3000);
   $("html, body").scrollTop(100);
 
-  // these are for the "what we do" section and enable for the icons to reveal content and viceversa
+  // these methods are for the "what we do" section and enable for the icons to reveal content and viceversa
   $(".text").hide();
-  $(".weDo-cont").click(function () {
+  $(".whatWeDo").click(function () {
     $(this).children(".text").slideToggle(500);
     $(this).children(".hide-img").fadeToggle(500);
   });
 
-  // this creates a hover effect using mouseover/mouseout
-  $(".port_desc").removeClass("here");
-  $(".port_cont").mouseover(function () {
-    $(this).children(".port_desc").addClass("here");
+  // these methods create a hover effect using mouseover/mouseout
+  $(".portfo").removeClass("here");
+  $(".portfo_cont").mouseover(function () {
+    $(this).children(".portfo").addClass("here");
   });
-  $(".port_cont").mouseleave(function () {
-    $(this).children(".port_desc").removeClass("here").fadeIn;
+  $(".portfo_cont").mouseleave(function () {
+    $(this).children(".portfo").removeClass("here").fadeIn;
   });
 
   // this entails the form submission conditional statements 
@@ -31,11 +31,11 @@ $(document).ready(function () {
     var data_1 = $("input:first").val();
     var data_2 = $("input#mail").val();
     if(data_1 && data_2 != ""  ){
-      $(".message").text("Thank you " + data_1 + " Your message has been received");
+      $(".message").text("Thank you, " + data_1  + "! Your message has been received.");
       $(".message").removeClass("error");
       $(".message").addClass("success");
     }else{
-      $(".message").text("Error!!! Please fill in all the details correctly");
+      $(".message").text("Error!!! Please ensure you have filled in all the details correctly");
       $(".message").addClass("error");
     }
 //This just prevents the page from reloading upon submission
